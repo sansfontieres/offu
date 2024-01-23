85d9aadf93f678a21748c55ed2ca7499f22d0966
Romi Hervier <romi@sansfontieres.com>
2024-01-23 20:42:42 +0100

    Use Xml.zig to parse metainfo.plist

--- src/MetaInfo.zig
+++ src/MetaInfo.zig
@@ -4,12 +4,13 @@
 //! so is MetaInfo
 //!
 //! [metainfo.plist]: https://unifiedfontobject.org/versions/ufo3/metainfo.plist/
-const std = @import("std");
-const xml = @import("libxml2.zig");
-
-const meta_info_default_creator = "com.sansfontieres.offu";
-
 const MetaInfo = @This();
+
+const std = @import("std");
+const Xml = @import("Xml.zig");
+
+const MetaInfoEnum = std.meta.FieldEnum(MetaInfo);
+const meta_info_default_creator = "com.sansfontieres.offu";
 
 /// The application or library that created the UFO. This should follow
 /// a reverse domain naming scheme. For example, org.robofab.ufoLib.
@@ -17,51 +18,70 @@ creator: ?[]const u8 = meta_info_default_creator,
 
 /// The major version number of the UFO format. 3 for UFO 3. Required.
 /// (and we only support UFO3 here)
-format_version: usize = 3,
+format_version: usize = undefined,
 
 /// Optional if 0
 format_version_minor: ?usize = null,
 
-test "serialize" {
-    // TODO
-    const xml_str =
-        \\<?xml version="1.0" encoding="UTF-8"?>
-        \\<!DOCTYPE plist PUBLIC "-//Apple Computer//DTD PLIST 1.0//EN"
-        \\"http://www.apple.com/DTDs/PropertyList-1.0.dtd">
-        \\<plist version="1.0">
-        \\<dict>
-        \\  <key>creator</key>
-        \\  <string>org.robofab.ufoLib</string>
-        \\  <key>formatVersion</key>
-        \\  <integer>3</integer>
-        \\  <key>formatVersionMinor</key>
-        \\  <integer>0</integer>
-        \\</dict>
-        \\</plist>
-    ;
-    const reader = xml.xmlReaderForMemory(
-        xml_str,
-        @intCast(xml_str.len),
-        null,
-        "utf-8",
-        0,
-    );
+const Error = error{
+    MalformedFile,
+    WrongFile,
+};
 
-    while (true) {
-        switch (xml.xmlTextReaderRead(reader)) {
-            -1 => return error.ParseFailed,
-            0 => break,
-            else => {
-                //
-            },
-        }
-    }
+// This is medieval
+pub fn nodeToField(
+    self: *MetaInfo,
+    key_node: Xml.Node,
+    value_node: Xml.Node,
+) !void {
+    const key = key_node.getContent() orelse return Xml.Error.EmptyElement;
 
-    if (xml.xmlTextReaderClose(reader) == -1) {
-        return error.ParseFailed;
+    // We recognize that this key/value exists, but we will replace
+    // it with meta_info_default_creator
+    if (std.mem.eql(u8, key, "creator")) return;
+
+    const value = value_node.getContent() orelse return Xml.Error.EmptyElement;
+
+    if (std.mem.eql(u8, key, "formatVersion")) {
+        self.format_version = try std.fmt.parseInt(usize, value, 10);
+    } else {
+        return error.UnknownField;
     }
 }
 
+// This is medieval
+pub fn initFromDoc(doc: *Xml) !MetaInfo {
+    const root_node = try doc.getRootElement();
+
+    if (!std.mem.eql(u8, root_node.getName(), "plist")) {
+        return Error.WrongFile;
+    }
+
+    const dict: ?Xml.Node = root_node.findChild("dict") orelse {
+        return Error.MalformedFile;
+    };
+
+    var meta_info = MetaInfo{};
+
+    var node_it = dict.?.iterateDict();
+    while (node_it.next()) |element| {
+        try meta_info.nodeToField(
+            element,
+            node_it.next() orelse return Xml.Error.NoValue,
+        );
+    }
+
+    return meta_info;
+}
+
 test "deserialize" {
-    // TODO
+    var doc = try Xml.fromFile("test_inputs/Untitled.ufo/metainfo.plist");
+    defer doc.deinit();
+    const meta_info = try initFromDoc(&doc);
+    try std.testing.expectEqualStrings(
+        meta_info_default_creator,
+        meta_info.creator.?,
+    );
+    try std.testing.expectEqual(3, meta_info.format_version);
+    try std.testing.expectEqual(null, meta_info.format_version_minor);
 }
